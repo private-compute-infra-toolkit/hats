790a466375b2d602f76a59244d6c20e9d8971074
Yu Ding <dingelish@google.com>
2024-05-21 16:30:39 -0700

    fix build script and submodules
    Since we moved to the hats repo and submodule layout is different, we need to make corresponding changes to build.sh. Also we fix a few bugs in the current build.sh and oak-on-prem branch, and use the release branch for kv-server
    
    Change-Id: Id21acfaeba164d3cce61d0b585b5ee101d8a8d94

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,30 +1,33 @@
+set -e
+
 readonly SCRIPTS_DIR="$(dirname "$0")"
 cd "$SCRIPTS_DIR"
 
 echo "BUILDING CLOUD HYPERVISOR..."
-pushd ../oak/oak_on_prem_cloud_hypervisor
+pushd ../submodules/oak/oak_on_prem_cloud_hypervisor
 rm -rf ./cloud-hypervisor-38.0 && \
   make && \
   sudo setcap cap_net_admin+ep target/cloud-hypervisor && \
-  cp target/cloud-hypervisor ../../cloud-hypervisor
+  cp target/cloud-hypervisor ../../../cloud-hypervisor
 popd
 
 printf "\nBUILDING KV SERVICE..."
-pushd ../protected-auction-key-value-service
+pushd ../submodules/kv-server
 ./builders/tools/bazel-debian build //components/data_server/server:server \
   --//:platform=local \
   --//:instance=local
-cp -f bazel-bin/components/data_server/server/server ../oak/oak_containers_system_image/target/kv-server
+mkdir -p ../../submodules/oak/oak_containers_system_image/target/
+cp -f bazel-bin/components/data_server/server/server ../../submodules/oak/oak_containers_system_image/target/kv-server
 popd
 
 printf "\nBUILDING OAK CONTAINERS KERNEL..."
-pushd ../oak/oak_containers_kernel
+pushd ../submodules/oak/oak_containers_kernel
 make clean && make && \
-  cp target/bzImage ../..
+  cp -f target/bzImage ../../../
 popd
 
 printf "\nBUILDING OAK CONTAINERS IMAGES..."
-pushd ../oak/oak_containers_system_image
+pushd ../submodules/oak/oak_containers_system_image
 nix develop --command ./build-old.sh && \
-  mv target/output.img ../..
+  mv target/output.img ../../../
 popd
