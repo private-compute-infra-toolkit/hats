bd4e1d94d9f67744fbc2a0757837632dac80ecb1
Abdulla Alwabel <alwabel@google.com>
2024-08-05 03:35:10 +0000

    Create SecretData to hold keys, the object ensures proper wipeout of secrets and reduce leakage.
    I looked around and the closest thing to what we want is in Tink library, however, the object is so complicated
    and tries to solve another problem (it marks memory containing sensitive data so coredump redacts them) - I just
    want to make sure that data are erased properly and disallow copying it by mistake.
    
    SecretData is an object to store sensitive data. The object helps minimizing data leakage, and
    ensure proper erasure that are often caused by optimization.
    to other functions. The usage of this object reduce the need to explicitly
    case/convert between data types as it can be constructed from strings and
    string_view's, it also return a pointer to the inner array to allow manual
    manipulation and to be passed to functions accepting arrays. Also, it returns
    a string_view to be passed to other functions accepting strings. This is a
    replacement to strings and vectors as the This is a replacement to strings
    and vectors as the underlying data might be copied to other location. It is
    not trivial to ensure proper erasure as implementation varies across
    platform.
    
    I will work on using this objects in all places that handles sensitive data.
    
    Bug: b/344012112
    Change-Id: I480c6016f4421666b7fa3f237f36798360982de0

---
+++ crypto/secret-data.cc
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "crypto/secret-data.h"
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "openssl/mem.h"
+
+namespace privacy_sandbox::crypto {
+
+SecretData::SecretData(size_t size)
+    : size_(size), actual_size_(size), data_(new uint8_t[size_]) {}
+
+SecretData::SecretData(absl::string_view data)
+    : size_(data.size()), actual_size_(size_) {
+  data_ = std::unique_ptr<uint8_t[]>(new uint8_t[size_]);
+  memcpy(GetData(), data.data(), size_);
+}
+
+SecretData::SecretData(const SecretData& other) { *this = other; }
+
+SecretData::SecretData(SecretData&& other) { *this = std::move(other); }
+
+SecretData& SecretData::operator=(const SecretData& other) {
+  if (this != &other) {
+    size_ = other.size_;
+    actual_size_ = other.actual_size_;
+    data_ = std::unique_ptr<uint8_t[]>(new uint8_t[size_]);
+    memcpy(GetData(), other.GetData(), GetSize());
+  }
+  return *this;
+}
+
+SecretData& SecretData::operator=(SecretData&& other) {
+  std::swap(data_, other.data_);
+  std::swap(size_, other.size_);
+  std::swap(actual_size_, other.actual_size_);
+  return *this;
+}
+
+SecretData::~SecretData() { Cleanse(); }
+
+uint8_t* SecretData::GetData() { return data_.get(); }
+
+const uint8_t* SecretData::GetData() const { return data_.get(); }
+
+size_t SecretData::GetSize() const { return size_; }
+
+absl::Status SecretData::Resize(size_t new_size) {
+  if (new_size > size_) {
+    return absl::InvalidArgumentError("Size increase is not supported.");
+  }
+  size_ = new_size;
+  return absl::OkStatus();
+}
+
+absl::string_view SecretData::GetStringView() const {
+  return absl::string_view(reinterpret_cast<const char*>(data_.get()), size_);
+}
+
+void SecretData::Cleanse() { OPENSSL_cleanse(data_.get(), actual_size_); }
+
+}  // namespace privacy_sandbox::crypto
