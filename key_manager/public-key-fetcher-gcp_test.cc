6e1b894143881b246adbe8aee8e63dc01d6a0cd9
Sida Chen <sidachen@google.com>
2024-09-05 00:56:21 +0000

    Implement Per Adtech public key endpoint
    The endpoint will do 2 things
    1. Merge all the public keys onprem or cloud into a single list.
    2. (TODO) Refresh the JSON file stored in Cloud bucket on an RPC call.
      The refresh part will be implemented in the next CL. And we will also
      provide terraform config to make sure it's deployed with CDN properly
      in GCP.
    
    Bug: 337904644
    Change-Id: Ib2fdb2589a97204b1372808bb46527eacc831e12

---
+++ key_manager/public-key-fetcher-gcp_test.cc
@@ -0,0 +1,150 @@
+// Copyright 2024 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "key_manager/public-key-fetcher-gcp.h"
+
+#include <utility>
+
+#include "absl/status/status_matchers.h"
+#include "absl/status/statusor.h"
+#include "gmock/gmock.h"
+#include "google/cloud/spanner/client.h"
+#include "google/cloud/spanner/mocks/mock_spanner_connection.h"
+#include "google/cloud/spanner/mocks/row.h"
+#include "google/cloud/status.h"
+#include "gtest/gtest.h"
+
+namespace privacy_sandbox::key_manager {
+namespace {
+
+using ::absl_testing::IsOkAndHolds;
+using ::absl_testing::StatusIs;
+using ::testing::Eq;
+using ::testing::FieldsAre;
+using ::testing::HasSubstr;
+using ::testing::Return;
+using ::testing::StrEq;
+using ::testing::UnorderedElementsAre;
+
+constexpr absl::string_view kGetLatestPublicKeysRowSchema = R"pb(
+  row_type: {
+    fields: {
+      name: "SecretId",
+      type: { code: INT64 }
+    }
+    fields: {
+      name: "PublicKey",
+      type: { code: STRING }
+    }
+    fields: {
+      name: "Origin",
+      type: { code: STRING }
+    }
+  })pb";
+// This test generates a DEK, pass it to the test KMS to return it to the
+// KeyFetcher. Encrypt some data with the DEK and have mock spanner return it.
+// The key fetcher should be able to decrypt the encrypted data.
+TEST(PublicKeyFetcherGcp, GetLatestPublicKeys) {
+  auto mock_result_set_source =
+      std::make_unique<google::cloud::spanner_mocks::MockResultSetSource>();
+
+  google::spanner::v1::ResultSetMetadata metadata;
+  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
+      kGetLatestPublicKeysRowSchema, &metadata));
+  EXPECT_CALL(*mock_result_set_source, Metadata())
+      .WillRepeatedly(Return(metadata));
+
+  // Test will take only one key for each origin. This happens when we have
+  // exactly same update timestamp for some origin.
+  EXPECT_CALL(*mock_result_set_source, NextRow())
+      .WillOnce(Return(google::cloud::spanner_mocks::MakeRow({
+          {"SecretId", google::cloud::spanner::Value(4)},
+          {"PublicKey", google::cloud::spanner::Value("data4-public")},
+          {"Origin", google::cloud::spanner::Value("origin")},
+      })))
+      .WillOnce(Return(google::cloud::spanner_mocks::MakeRow({
+          {"SecretId", google::cloud::spanner::Value(2)},
+          {"PublicKey", google::cloud::spanner::Value("data2-public")},
+          {"Origin", google::cloud::spanner::Value("origin2")},
+      })))
+      .WillOnce(Return(google::cloud::spanner_mocks::MakeRow({
+          {"SecretId", google::cloud::spanner::Value(3)},
+          {"PublicKey", google::cloud::spanner::Value("data3-public")},
+          {"Origin", google::cloud::spanner::Value("origin")},
+      })))
+      .WillOnce(Return(google::cloud::spanner::Row()));
+
+  auto mock_connection =
+      std::make_shared<google::cloud::spanner_mocks::MockConnection>();
+
+  EXPECT_CALL(*mock_connection, ExecuteQuery)
+      .WillOnce(
+          [&mock_result_set_source](
+              const google::cloud::spanner::Connection::SqlParams& sql_params)
+              -> google::cloud::spanner::RowStream {
+            return google::cloud::spanner::RowStream(
+                std::move(mock_result_set_source));
+          });
+
+  google::cloud::spanner::Client spanner_client(mock_connection);
+  std::unique_ptr<PublicKeyFetcher> key_fetcher =
+      PublicKeyFetcherGcp::Create(std::move(spanner_client));
+
+  EXPECT_THAT(
+      key_fetcher->GetLatestPublicKeys(),
+      IsOkAndHolds(UnorderedElementsAre(
+          FieldsAre(
+              /*key_id=*/4, /*public_key=*/"data4-public", /*origin=*/"origin"),
+          FieldsAre(
+              /*key_id=*/2, /*public_key=*/"data2-public",
+              /*origin=*/"origin2"))));
+}
+
+TEST(PublicKeyFetcherGcp, Failure) {
+  auto mock_result_set_source =
+      std::make_unique<google::cloud::spanner_mocks::MockResultSetSource>();
+
+  google::spanner::v1::ResultSetMetadata metadata;
+  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
+      kGetLatestPublicKeysRowSchema, &metadata));
+  EXPECT_CALL(*mock_result_set_source, Metadata())
+      .WillRepeatedly(Return(metadata));
+
+  EXPECT_CALL(*mock_result_set_source, NextRow())
+      .WillOnce(Return(google::cloud::Status(
+          google::cloud::StatusCode::kUnknown, "unknown error")));
+
+  auto mock_connection =
+      std::make_shared<google::cloud::spanner_mocks::MockConnection>();
+
+  EXPECT_CALL(*mock_connection, ExecuteQuery)
+      .WillOnce(
+          [&mock_result_set_source](
+              const google::cloud::spanner::Connection::SqlParams& sql_params)
+              -> google::cloud::spanner::RowStream {
+            return google::cloud::spanner::RowStream(
+                std::move(mock_result_set_source));
+          });
+
+  google::cloud::spanner::Client spanner_client(mock_connection);
+  std::unique_ptr<PublicKeyFetcher> key_fetcher =
+      PublicKeyFetcherGcp::Create(std::move(spanner_client));
+
+  EXPECT_THAT(key_fetcher->GetLatestPublicKeys(),
+              StatusIs(absl::StatusCode::kUnknown, HasSubstr("unknown")));
+}
+
+}  // namespace
+
+}  // namespace privacy_sandbox::key_manager
---
+++ public_key/public-key-server.cc
@@ -0,0 +1,168 @@
+// Copyright 2024 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "public_key/public-key-server.h"
+
+#include <thread>
+
+#include "absl/log/log.h"
+#include "absl/strings/escaping.h"
+#include "absl/strings/str_cat.h"
+#include "curl/curl.h"
+#include "google/protobuf/empty.pb.h"
+#include "google/protobuf/util/json_util.h"
+#include "grpcpp/ext/proto_server_reflection_plugin.h"
+#include "grpcpp/security/server_credentials.h"
+#include "grpcpp/server.h"
+#include "grpcpp/server_builder.h"
+#include "grpcpp/server_context.h"
+#include "grpcpp/support/status.h"
+#include "key_manager/public-key-fetcher.h"
+#include "public_key/proto/public_key_service.pb.h"
+
+namespace privacy_sandbox::public_key_service {
+namespace {
+size_t ResponseHandler(char* contents, size_t byte_size, size_t num_bytes,
+                       std::string* output) {
+  output->append(contents, byte_size * num_bytes);
+  return byte_size * num_bytes;
+}
+absl::StatusOr<std::string> DownloadPublicKeys(const std::string& url) {
+  CURL* curl = curl_easy_init();
+  if (curl == nullptr) {
+    return absl::UnknownError("null curl_api is not allowed");
+  }
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, ResponseHandler);
+  std::string certificate;
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &certificate);
+  if (CURLcode res = curl_easy_perform(curl); res != CURLE_OK) {
+    curl_easy_cleanup(curl);
+    return absl::UnknownError(
+        absl::StrCat("Error downloading public keys from '", url, "'"));
+  }
+  curl_easy_cleanup(curl);
+  return certificate;
+}
+absl::StatusOr<ListPublicKeysResponse> ParseKeyJson(
+    absl::string_view content,
+    privacy_sandbox::public_key_service::ExecutionEnvironment
+        execution_environment) {
+  ListPublicKeysResponse resp;
+  if (!google::protobuf::util::JsonStringToMessage(content, &resp).ok())
+    return absl::UnknownError(absl::StrCat(
+        "failed to parse public keys from upstream json with status"));
+  for (auto& public_key : *resp.mutable_public_keys()) {
+    public_key.set_execution_environment(execution_environment);
+  }
+  return resp;
+}
+}  // namespace
+
+grpc::Status PublicKeyServer::ListPublicKeys(
+    grpc::ServerContext* context, const google::protobuf::Empty* request,
+    ListPublicKeysResponse* response) {
+  absl::StatusOr<std::string> aws_public_key;
+  absl::StatusOr<std::string> gcp_public_key;
+  absl::StatusOr<std::vector<privacy_sandbox::key_manager::PerOriginPublicKey>>
+      onprem_public_key;
+  std::thread aws_download([&]() {
+    aws_public_key = DownloadPublicKeys(options_.aws_key_endpoint);
+  });
+  std::thread gcp_download([&]() {
+    gcp_public_key = DownloadPublicKeys(options_.gcp_key_endpoint);
+  });
+  std::thread onprem_download(
+      [&]() { onprem_public_key = fetcher_->GetLatestPublicKeys(); });
+  aws_download.join();
+  gcp_download.join();
+  onprem_download.join();
+  if (!aws_public_key.ok() || !gcp_public_key.ok() || !onprem_public_key.ok()) {
+    return grpc::Status(
+        grpc::StatusCode::UNAVAILABLE,
+        absl::StrCat("failed to fetch some public keys from upstream: AWS: ",
+                     aws_public_key.status(), " GCP: ", gcp_public_key.status(),
+                     " ONPREM: ", onprem_public_key.status()));
+  }
+
+  absl::StatusOr<ListPublicKeysResponse> aws_keys =
+      ParseKeyJson(*aws_public_key, EXECUTION_ENVIRONMENT_AWS);
+  if (!aws_keys.ok())
+    // Upstream breaks the json format assumption and therefore it's a severe
+    // issue.
+    return grpc::Status(grpc::StatusCode::INTERNAL,
+                        absl::StrCat("failed to parse AWS json with error ",
+                                     aws_keys.status()));
+  absl::StatusOr<ListPublicKeysResponse> gcp_keys =
+      ParseKeyJson(*gcp_public_key, EXECUTION_ENVIRONMENT_GCP);
+  if (!gcp_keys.ok())
+    // Upstream breaks the json format assumption and therefore it's a severe
+    // issue.
+    return grpc::Status(grpc::StatusCode::INTERNAL,
+                        absl::StrCat("failed to parse GCP json with error ",
+                                     gcp_keys.status()));
+  (*response->mutable_public_keys())
+      .Add((*aws_keys).public_keys().begin(), (*aws_keys).public_keys().end());
+  (*response->mutable_public_keys())
+      .Add((*gcp_keys).public_keys().begin(), (*gcp_keys).public_keys().end());
+  for (const privacy_sandbox::key_manager::PerOriginPublicKey& key :
+       (*onprem_public_key)) {
+    PublicKey* onprem_key = response->add_public_keys();
+    // Example existing key ID format:
+    // 7F00000000000000
+    // The existing keys are stored as HEX string in the database as STRING(50).
+    onprem_key->set_key_id(absl::StrCat(absl::Hex(key.key_id)));
+    // Example existing public key format:
+    // The existing public keys are stored in non websafe base64 format.
+    // The onprem keys are stored in HEX format
+    // Therefore, we should do the conversion.
+    std::string public_key_bytes;
+    if (!absl::HexStringToBytes(key.public_key, &public_key_bytes))
+      return grpc::Status(
+          grpc::StatusCode::INTERNAL,
+          "failed to convert onprem public key from hex to bytes");
+    std::string public_key_base64_encoded =
+        absl::Base64Escape(public_key_bytes);
+    onprem_key->set_public_key(public_key_base64_encoded);
+    onprem_key->set_adtech_origin(key.origin);
+    onprem_key->set_execution_environment(EXECUTION_ENVIRONMENT_ONPREM);
+  }
+
+  return grpc::Status::OK;
+}
+
+PublicKeyServer::PublicKeyServer(
+    const PublicKeyServerOptions& options,
+    std::unique_ptr<privacy_sandbox::key_manager::PublicKeyFetcher> fetcher)
+    : options_(options), fetcher_(std::move(fetcher)) {}
+
+void CreateAndStartPublicKeyServer(
+    const PublicKeyServerOptions& options,
+    std::unique_ptr<privacy_sandbox::key_manager::PublicKeyFetcher> fetcher) {
+  // CURL global init cannot be called in parallel. We force the call here to
+  // prevent curl_easy_init() function from calling this function in separate
+  // thread.
+  curl_global_init(CURL_GLOBAL_SSL);
+  std::string server_address = absl::StrCat("0.0.0.0:", options.port);
+  PublicKeyServer public_key_server(options, std::move(fetcher));
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  std::unique_ptr<grpc::Server> server =
+      grpc::ServerBuilder()
+          .AddListeningPort(server_address, grpc::InsecureServerCredentials())
+          .RegisterService(&public_key_server)
+          .BuildAndStart();
+  LOG(INFO) << "Server listening on " << server_address;
+  server->Wait();
+}
+}  // namespace privacy_sandbox::public_key_service
