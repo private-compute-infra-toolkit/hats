b86540de89243e7dbfd5756c0c61bce889a0a53d
Kevin Garbe <kgarbe@google.com>
2024-09-11 23:48:22 +0000

    Add wrapper for bazel-debian that applies patches
    Patches apply locally (even within cbuild), so also revert afterwards
    Patches common and python
    
    TODO: Consider putting in a better directory, since google_internal is
    internal only, and this is meant to replace bazel.
    
    Once common is fixed (no patch) and bazel-debian is default, neither
    patch will be needed
    
    Use example: "google_internal/bazel-debian.sh test //..."
    
    Bug: b/351201455
    Change-Id: I658467152165c1721c31eec301363b67c9edddff

---
+++ google_internal/bazel-debian.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# shellcheck disable=all
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# environment variables supported by cbuild (all optional):
+#     WORKSPACE                    Set the path to the workspace (repo root)
+#     AWS_ACCESS_KEY_ID            AWS auth token
+#     AWS_SECRET_ACCESS_KEY        AWS auth token
+#     BAZEL_STARTUP_ARGS           Additional startup arguments to pass to bazel invocations
+#     BAZEL_EXTRA_ARGS             Additional command arguments to pass to bazel invocations
+#     EXTRA_DOCKER_RUN_ARGS        Additional arguments to pass to docker run invocations
+
+set -o pipefail
+set -o errexit
+
+# TODO: Consider which directory to put this.
+# internal will be excluded, but this is also ideally temporary
+
+# Exists as a wrapper around builders/tools/bazel-debian
+# Ideally, the wrapper isn't needed.
+# The wrapper is to allow it to run on everything.
+# 1. Apply patches to common, else it can't build the git module
+#    Once common no longer needs the patch (c versions), this won't be needed
+# 2. Build python related things (tar_pkg), avoiding root issues
+#    Once bazel-debian is the default dev invocation, WORKSPACE can register the
+#    python toolchain by default (which uses bazel-debian python path),
+#    and so the patch won't be needed
+
+# Usage: 'google_internal/bazel-debian test //...'
+
+source "patches/apply_patches.sh"
+patches::apply_common
+patches::apply_python
+
+builders/tools/bazel-debian "$@"
+
+patches::revert_common
+patches::revert_python
