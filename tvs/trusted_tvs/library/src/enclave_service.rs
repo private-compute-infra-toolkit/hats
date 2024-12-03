500d8d6c4140cccdf4de6e1846b72bf55fda38f9
Abdulla Alwabel <alwabel@google.com>
2024-12-03 06:42:41 +0000

    Add unit-test for enclave_service module.
    Bug: b/344012112
    Change-Id: Iaa40c9260e9a284fe5306f54fc4fbedfb55092cf

--- tvs/trusted_tvs/library/src/enclave_service.rs
+++ tvs/trusted_tvs/library/src/enclave_service.rs
@@ -235,3 +235,212 @@ impl KeyProvider for KeyFetcherService {
         Ok(secret.to_vec())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crypto::P256Scalar;
+    use oak_proto_rust::oak::attestation::v1::TcbVersion;
+    use prost::Message;
+    use tvs_proto::privacy_sandbox::tvs::{
+        stage0_measurement, AmdSev, AppraisalPolicies, AppraisalPolicy, Measurement,
+        Signature as PolicySignature, Stage0Measurement,
+    };
+    use tvs_trusted_client::TvsClient;
+
+    fn get_good_evidence() -> Vec<u8> {
+        include_bytes!("../../../test_data/good_evidence.binarypb").to_vec()
+    }
+
+    fn get_malformed_evidence() -> Vec<u8> {
+        include_bytes!("../../../test_data/malformed_evidence.binarypb").to_vec()
+    }
+
+    fn get_genoa_vcek() -> Vec<u8> {
+        include_bytes!("../../../test_data/vcek_genoa.crt").to_vec()
+    }
+
+    fn default_appraisal_policies() -> Vec<u8> {
+        let policies = AppraisalPolicies {
+            policies: vec![AppraisalPolicy{
+                measurement: Some(Measurement {
+                    stage0_measurement: Some(Stage0Measurement{
+                        r#type: Some(stage0_measurement::Type::AmdSev(AmdSev{
+                            sha384: "de654ed1eb03b69567338d357f86735c64fc771676bcd5d05ca6afe86f3eb9f7549222afae6139a8d282a34d09d59f95".to_string(),
+                            min_tcb_version: Some(TcbVersion{
+                                boot_loader: 7,
+                                microcode: 62,
+                                snp: 15,
+                                tee: 0,
+                            }),
+                        })),
+                    }),
+                    kernel_image_sha256: "442a36913e2e299da2b516814483b6acef11b63e03f735610341a8561233f7bf".to_string(),
+                    kernel_setup_data_sha256: "68cb426afaa29465f7c71f26d4f9ab5a82c2e1926236648bec226a8194431db9".to_string(),
+                    init_ram_fs_sha256: "3b30793d7f3888742ad63f13ebe6a003bc9b7634992c6478a6101f9ef323b5ae".to_string(),
+                    memory_map_sha256: "4c985428fdc6101c71cc26ddc313cd8221bcbc54471991ec39b1be026d0e1c28".to_string(),
+                    acpi_table_sha256: "a4df9d8a64dcb9a713cec028d70d2b1599faef07ccd0d0e1816931496b4898c8".to_string(),
+                    kernel_cmd_line_regex: "^ console=ttyS0 panic=-1 brd.rd_nr=1 brd.rd_size=10000000 brd.max_part=1 ip=10.0.2.15:::255.255.255.0::eth0:off$".to_string(),
+                    system_image_sha256: "e3ded9e7cfd953b4ee6373fb8b412a76be102a6edd4e05aa7f8970e20bfc4bcd".to_string(),
+                    container_binary_sha256:"bf173d846c64e5caf491de9b5ea2dfac349cfe22a5e6f03ad8048bb80ade430c".to_string(),
+
+                }),
+                signature: vec![PolicySignature{
+                    signature: "003cfc8524266b283d4381e967680765bbd2a9ac2598eb256ba82ba98b3e23b384e72ad846c4ec3ff7b0791a53011b51d5ec1f61f61195ff083c4a97d383c13c".to_string(),
+                    signer: "".to_string(),
+                    },
+                    ],
+            }],
+        };
+        let mut buf: Vec<u8> = Vec::with_capacity(1024);
+        policies.encode(&mut buf).unwrap();
+        buf
+    }
+
+    #[test]
+    fn verify_report_successful() {
+        let tvs_private_key = P256Scalar::generate();
+        let client_private_key = P256Scalar::generate();
+        let mut service = EnclaveService::new().unwrap();
+
+        // Provision the TVS keys.
+        service
+            .provision_keys(ProvisionKeysRequest {
+                private_key: tvs_private_key.bytes().to_vec(),
+            })
+            .unwrap();
+
+        // Load the appraisal policies.
+        service
+            .load_appraisal_policies(LoadAppraisalPoliciesRequest {
+                policies: default_appraisal_policies(),
+            })
+            .unwrap();
+
+        // Register a user.
+        service
+            .register_or_update_user(RegisterOrUpdateUserRequest {
+                id: 1,
+                authentication_key: client_private_key.compute_public_key().to_vec(),
+                secret: b"secret".to_vec(),
+            })
+            .unwrap();
+
+        let mut tvs_client = TvsClient::new(
+            &client_private_key.bytes(),
+            &tvs_private_key.compute_public_key(),
+        )
+        .unwrap();
+
+        // Create a session (do handshake).
+        let create_session_response = service
+            .create_session(CreateSessionRequest {
+                binary_message: tvs_client.build_initial_message().unwrap(),
+            })
+            .unwrap();
+
+        let session_id = create_session_response.session_id;
+
+        tvs_client
+            .process_handshake_response(&create_session_response.binary_message)
+            .unwrap();
+
+        // Send the attestation report and get the secret.
+        let do_command_response = service
+            .do_command(DoCommandRequest {
+                session_id: session_id.clone(),
+                binary_message: tvs_client
+                    .build_verify_report_request(
+                        &get_good_evidence(),
+                        &get_genoa_vcek(),
+                        /*application_signing_key=*/
+                        "cf8d805ed629f4f95d20714a847773b3e53d3d8ab155e52c882646f702a98ce8",
+                    )
+                    .unwrap(),
+            })
+            .unwrap();
+
+        assert_eq!(
+            tvs_client
+                .process_response(&do_command_response.binary_message)
+                .unwrap(),
+            b"secret"
+        );
+
+        service
+            .terminate_session(TerminateSessionRequest { session_id })
+            .unwrap();
+    }
+
+    #[test]
+    fn verify_report_invalid_report_error() {
+        let tvs_private_key = P256Scalar::generate();
+        let client_private_key = P256Scalar::generate();
+        let mut service = EnclaveService::new().unwrap();
+
+        // Provision the TVS keys.
+        service
+            .provision_keys(ProvisionKeysRequest {
+                private_key: tvs_private_key.bytes().to_vec(),
+            })
+            .unwrap();
+
+        // Load the appraisal policies.
+        service
+            .load_appraisal_policies(LoadAppraisalPoliciesRequest {
+                policies: default_appraisal_policies(),
+            })
+            .unwrap();
+
+        // Register a user.
+        service
+            .register_or_update_user(RegisterOrUpdateUserRequest {
+                id: 1,
+                authentication_key: client_private_key.compute_public_key().to_vec(),
+                secret: b"secret".to_vec(),
+            })
+            .unwrap();
+
+        let mut tvs_client = TvsClient::new(
+            &client_private_key.bytes(),
+            &tvs_private_key.compute_public_key(),
+        )
+        .unwrap();
+
+        // Create a session (do handshake).
+        let create_session_response = service
+            .create_session(CreateSessionRequest {
+                binary_message: tvs_client.build_initial_message().unwrap(),
+            })
+            .unwrap();
+
+        let session_id = create_session_response.session_id;
+
+        tvs_client
+            .process_handshake_response(&create_session_response.binary_message)
+            .unwrap();
+
+        match service.do_command(DoCommandRequest {
+            session_id: session_id.clone(),
+            binary_message: tvs_client
+                .build_verify_report_request(
+                    &get_malformed_evidence(),
+                    &get_genoa_vcek(),
+                    /*application_signing_key=*/
+                    "cf8d805ed629f4f95d20714a847773b3e53d3d8ab155e52c882646f702a98ce8",
+                )
+                .unwrap(),
+        }) {
+            Ok(_) => panic!("verify_command() should fail."),
+            Err(e) => {
+                assert!(e
+                    .to_string()
+                    .contains("Failed to verify report. No matching appraisal policy found"))
+            }
+        }
+
+        service
+            .terminate_session(TerminateSessionRequest { session_id })
+            .unwrap();
+    }
+}
