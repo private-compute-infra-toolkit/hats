f418f0347d05c97a38d48d2bbe3be2ceff2fae1d
Kevin Garbe <kgarbe@google.com>
2025-04-22 16:38:58 +0000

    Scripts for updating and checking binary hashes
    Generates and updates hash storage files
    - Enclave binary
    - All system images in client/system_image
    
    Bug: b/410633945
    Change-Id: Ib8662c27d283d097f8c1a73061a59ed3257b71d9

---
+++ client/scripts/update-hashes.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Copyright 2025 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###### Compute hashes and store them in hash files
+
+set -e
+set -o pipefail
+
+TEST_APP_DIR="$(dirname "$0")"
+readonly TEST_APP_DIR
+PREBUILT_DIR="$(readlink -f "$TEST_APP_DIR/../prebuilt")"
+readonly PREBUILT_DIR
+mkdir -p "$PREBUILT_DIR"
+CLIENT_DIR="$(dirname "$(readlink -fm "${TEST_APP_DIR}")")"
+readonly CLIENT_DIR
+cd "$TEST_APP_DIR"
+
+# Files
+readonly RUNTIME_HASH_FILE="${CLIENT_DIR}/trusted_application/enclave_app/SHA256SUMS"
+readonly SYSTEM_IMAGE_HASH_FILE="${CLIENT_DIR}/system_image/SHA256SUMS"
+
+source ../scripts/build-lib.sh
+
+#### Group 1: Container binary: Runtime
+build_test_application_container_bundle_tar "$PREBUILT_DIR"
+RUNTIME_HASH=$(sha256sum "${PREBUILT_DIR}/bundle.tar" | cut -d " " -f 1)
+echo "Runtime hash: ${RUNTIME_HASH}"
+
+# Update hash file
+touch "${RUNTIME_HASH_FILE}"
+sed -i '/container_binary$/d' "${RUNTIME_HASH_FILE}"
+echo "${RUNTIME_HASH} container_binary" >> "${RUNTIME_HASH_FILE}"
+
+#### Group 2: Various system images
+
+# Oak syslogd and a BUILD file are needed. This will use artifact if it exists.
+build_oak_containers_syslogd "${PREBUILT_DIR}"
+
+# Get suffixes of images from the BUILD file
+mapfile -t IMAGE_SUFFIXES < <(grep 'name = "hats_system_image_' < "${CLIENT_DIR}/system_image/BUILD" | cut -d '"' -f 2 | cut -c19-)
+
+# Compute hashes
+IMAGE_HASHES=()
+for SUFFIX in "${IMAGE_SUFFIXES[@]}"; do
+  echo "computing system image for ${SUFFIX}"
+  build_hats_containers_images "$PREBUILT_DIR" "${SUFFIX}"
+  HASH=$(sha256sum "${PREBUILT_DIR}/hats_system_image.tar.xz" | cut -d " " -f 1)
+  echo "System hash for ${SUFFIX}: ${HASH}"
+  IMAGE_HASHES+=("${HASH}")
+done
+
+# Update hash file
+touch "${SYSTEM_IMAGE_HASH_FILE}"
+sed -i '/hats_system_image_/d' "${SYSTEM_IMAGE_HASH_FILE}"
+for index in "${!IMAGE_SUFFIXES[@]}"; do
+  echo "${IMAGE_HASHES[$index]} hats_system_image_${IMAGE_SUFFIXES[$index]}" >> "${SYSTEM_IMAGE_HASH_FILE}"
+done
